6ffeb965a1066695c8673e54d3fafc9751cc127d
unknown <martinarielle159@gmail.com>
2025-01-03 21:53:37 -0500

    Getting started with base code

---
+++ index.js
@@ -0,0 +1,10 @@
+// fetch = Function used for making HTTP requests to fetch resources.
+//         (JSON style DataTransfer, images, files)
+//         Simplifies asynchronous data fetching in Javascript and 
+//         used for interacting with APIs to retrieve and send
+//         data asynchronously over the web.
+//         fetch(URL, {options}) 
+
+fetchData();
+
+async function 
