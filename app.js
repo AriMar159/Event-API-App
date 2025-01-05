282da71fe1572fe31bb4f0c599e5d93493a28cfd
unknown <martinarielle159@gmail.com>
2025-01-04 20:14:32 -0500

    Put comments in app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 if (eventName !== '') {
-
+//Query Event Brite API
     eventbrite.queryAPI(eventName, category)
         .then (events => {
-
+                //Check for events
             const eventList = events.events.events;
             if (eventList.length > 0) {
-
+                //Print the events
                 ui.displayEvents(events.events);
 
             } else {
-
+                    //There are no events, print a message
                 ui.printMessage('No Results Found', 'text-center alert alert-danger mt-4');  
             }         
         })
 }else{
-
+        //Print a message
     ui.printMessage('Add an Event or City',  'text-center alert alert-danger mt-4');
 }
